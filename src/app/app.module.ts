2e9bcc3843b01e33c07ff2fa2f90fa2b36bdea22
FranciscoGongora0101 <jose.fco01@live.com.mx>
2020-06-16 17:11:26 -0500

    E2 A4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { VistasComponent } from './vistas/vistas.component';
 import { ListComponent } from './list/list.component';
+import { ModalComponent } from './modal/modal.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     VistasComponent,
-    ListComponent
+    ListComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Automovil } from 'src/assets/models';
 import { AUTOMOVILES } from 'src/assets/data';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-list',
@@ -8,13 +9,39 @@ import { AUTOMOVILES } from 'src/assets/data';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
-
+  autoSeleccionado: Automovil;
   autos: Automovil[];
-  constructor() { }
+
+  closeResult="";
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
     this.autos = AUTOMOVILES;
   }
 
+  onSelect(auto: Automovil){
+    this.autoSeleccionado= auto;
+
+  }
+  open(content, auto: Automovil) {
+    this.autoSeleccionado= auto;
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
+
+
+
